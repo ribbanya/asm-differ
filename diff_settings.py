1d5c8de2ec065907c4d535379b08758849898491
BR- <wishingwellwellwishing@hotmail.com>
2022-08-17 21:39:48 -0400

    Apply custom `melee` patch

--- diff_settings.py
+++ diff_settings.py
@@ -1,11 +1,10 @@
 def apply(config, args):
-    config["baseimg"] = "target.bin"
-    config["myimg"] = "source.bin"
-    config["mapfile"] = "build.map"
-    config["source_directories"] = ["."]
-    # config["show_line_numbers_default"] = True
-    # config["arch"] = "mips"
-    # config["map_format"] = "gnu" # gnu, mw, ms
-    # config["build_dir"] = "build/" # only needed for mw and ms map format
+    config["baseimg"] = "expected/build/ssbm.us.1.2/main.dol"
+    config["myimg"] = "build/ssbm.us.1.2/main.dol"
+    config["mapfile"] = "build/ssbm.us.1.2/GALE01.map"
+    config["source_directories"] = ["src", "include", "asm"]
+    config["arch"] = "ppc"
+    config["map_format"] = "mw" # gnu, mw, ms
+    config["build_dir"] = "build/ssbm.us.1.2/src" # only needed for mw and ms map format
     # config["makeflags"] = []
-    # config["objdump_executable"] = ""
+    config["objdump_executable"] = "/opt/devkitpro/devkitPPC/bin/powerpc-eabi-objdump.exe"
